a479043a40fd7ca9a03449c8cd90ac981af28d64
George Borisov <git@gir.me.uk>
2018-04-04 23:41:43 +0100

    attach splat to request req.splat

--- tests/spec/App.spec.js
+++ tests/spec/App.spec.js
@@ -13,6 +13,7 @@ const lib = require('../../index.js');
 describe('lib/App.js', function () {
     const port = 10001;
     const httpRequest = client(port);
+    const path = '/test/foofoo/bar/baz';
     var app;
 
     it('creates HTTP server when called with no options', function () {
@@ -27,10 +28,11 @@ describe('lib/App.js', function () {
         const m = k.toUpperCase();
 
         it(`registers ${m} handler`, function (done) {
-            app[k]('/test/:foo', function (req, res) {
+            app[k]('/test/:foo/*', function (req, res) {
                 expect(req.method).to.equal(m);
-                expect(req.url).to.equal('/test/foofoo');
+                expect(req.url).to.equal(path);
                 expect(req.params.foo).to.equal('foofoo');
+                expect(req.splat).to.equal('bar/baz');
 
                 req.on('data', function (buffer) {
                     expect(buffer.toString()).to.equal('bar');
@@ -39,7 +41,7 @@ describe('lib/App.js', function () {
                 res.end('foo');
             });
 
-            httpRequest({ method: m, path: '/test/foofoo' }, 'bar', function (res, data) {
+            httpRequest({ method: m, path }, 'bar', function (res, data) {
                 expect(res.statusCode).to.equal(200);
                 expect(data).to.equal('foo');
                 done();
@@ -52,7 +54,7 @@ describe('lib/App.js', function () {
         var semaphore = 2;
 
         function mw (req, res, next) {
-            expect(req.url).to.equal('/test/foofoo');
+            expect(req.url).to.equal(path);
             expect(typeof res.end).to.equal('function');
             expect(typeof next).to.equal('function');
 
@@ -66,7 +68,7 @@ describe('lib/App.js', function () {
 
         expect(app._middleware.length).to.equal(2);
 
-        httpRequest({ method: 'GET', path: '/test/foofoo' }, undefined, function (res, data) {
+        httpRequest({ method: 'GET', path }, undefined, function (res, data) {
             expect(res.statusCode).to.equal(200);
             expect(data).to.equal('foo');
             expect(semaphore).to.equal(0);
@@ -77,7 +79,7 @@ describe('lib/App.js', function () {
     });
 
     it('returns 404 in strange method', function (done) {
-        httpRequest({ method: 'ACL', path: '/test/foofoo' }, undefined, function (res, data) {
+        httpRequest({ method: 'ACL', path }, undefined, function (res, data) {
             expect(res.statusCode).to.equal(404);
             expect(typeof data).to.equal('string');
             done();
